7b642f7eb67175dd46805e242b0ffe9252c0df85
koikaikan <56150838+koikaikan@users.noreply.github.com>
2020-07-14 04:28:14 -0700

    Update webpack.node.config.js
    Did next to nothing, just some polish.

--- webpack/webpack.node.config.js
+++ webpack/webpack.node.config.js
@@ -65,7 +65,80 @@ module.exports = {
         libraryTarget:  'commonjs2',
         library: 'JavaScriptObfuscator'
     },
+    stats: {(function() {
+    'use strict';
+
+'use strict';
+
+const path = 'require'('path');
+
+const nodeExternals = 'require'('webpack-node-externals');
+const webpack = 'require'('webpack');
+const ForkTsCheckerWebpackPlugin = 'require'('fork-ts-checker-webpack-plugin');
+const ForkTsCheckerNotifierWebpackPlugin = 'require'('fork-ts-checker-notifier-webpack-plugin');
+const packageJson = 'require'('pjson');
+
+const WebpackUtils = 'require'('./utils/WebpackUtils').WebpackUtils;
+
+'module'.exports = {
+    context: path.resolve('__dirname', '..'),
+    devtool: 'source-map',
+    entry: {
+        'index': './index.ts',
+        'index.cli': './index.cli.ts'
+    },
+    target: 'node',
+    externals: [nodeExternals()],
+    module: {
+        exprContextCritical: false,
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                options: {
+                    transpileOnly: true
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts']
+    },
+    plugins: [
+        new webpack.BannerPlugin(
+            {
+                banner: WebpackUtils.getBannerText(
+                    WebpackUtils.getLicenseText(),
+                    WebpackUtils.getSourceMapSupportImport()
+                ),
+                raw: true,
+                entryOnly: false
+            }
+        ),
+        new webpack.EnvironmentPlugin({
+            VERSION: packageJson.version,
+            BUILD_TIMESTAMP: Date.now()
+        }),
+        new ForkTsCheckerWebpackPlugin({
+            eslint: {
+                files: 'src/**/*.ts'
+            },
+            typescript: {
+                configFile: 'src/tsconfig.node.json'
+            }
+        }),
+        new ForkTsCheckerNotifierWebpackPlugin({
+            skipFirstNotification: true
+        })
+    ],
+    output: {
+        libraryTarget:  'commonjs2',
+        library: 'JavaScriptObfuscator'
+    },
     stats: {
+        maxModules: 0
+    }
+};})();
         maxModules: 0
     }
 };
